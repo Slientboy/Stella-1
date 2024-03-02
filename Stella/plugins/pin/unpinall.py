ab65a13e7b5fe06b2cc22685b256d67effe496be
Slient Boy <83143204+Slientboy@users.noreply.github.com>
2024-03-02 21:30:52 +0500

    Update unpinall.py

--- Stella/plugins/pin/unpinall.py
+++ Stella/plugins/pin/unpinall.py
@@ -15,8 +15,8 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from pyrogram import filters
-from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
+from hydrogram import filters
+from hydrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup)
 from Stella import StellaCli
 from Stella.helper import custom_filter
