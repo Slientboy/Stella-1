5abc77d0fffc1db77da18f5e27a31266fc96269d
Slient Boy <83143204+Slientboy@users.noreply.github.com>
2024-03-02 22:34:06 +0500

    Update __main__.py

--- Stella/__main__.py
+++ Stella/__main__.py
@@ -67,7 +67,7 @@ print(HIDDEN_MOD)
 
 if __name__ == "__main__":
     scheduler.start()
-    StellaCli.start()
+    StellaCli.run()
 print("the bot starting")
 #StellaCli.start()
 #print("client working")
