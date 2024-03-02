773838ddaf359fb7c814c2fdff8a5786f4fdf190
Slient Boy <83143204+Slientboy@users.noreply.github.com>
2024-03-02 21:20:46 +0500

    Update reset_welcome.py

--- Stella/plugins/greeting/reset_welcome.py
+++ Stella/plugins/greeting/reset_welcome.py
@@ -16,7 +16,7 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-from pyrogram.methods import password
+from hydrogram.methods import password
 from Stella import StellaCli
 from Stella.database.welcome_mongo import UnSetWelcome
 from Stella.helper import custom_filter
