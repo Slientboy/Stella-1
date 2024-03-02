22ee67139a61b23b13ed4821615834068acb401c
Slient Boy <83143204+Slientboy@users.noreply.github.com>
2024-03-02 21:38:44 +0500

    Update chat_status.py

--- Stella/helper/chat_status.py
+++ Stella/helper/chat_status.py
@@ -18,7 +18,7 @@
 
 from typing import List, Union
 
-from pyrogram.types import Message
+from hydrogram.types import Message
 from Stella import (BOT_ID, GROUP_ANONYMOUS_BOT, SUDO_USERS, StellaCli,
                     TELEGRAM_SERVICES_IDs)
 from Stella.database.connection_mongo import GetConnectedChat
